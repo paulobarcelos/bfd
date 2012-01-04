0de5f594cefa929f523b5c052060598874628cb7
Paulo Barcelos <paulo@azucrina.org>
2012-01-04 01:34:48 +0100

    started added captions

--- exhibition/en/index.php
+++ exhibition/en/index.php
@@ -1,6 +1,16 @@
 <?php require('header.php');?>
 
 <ul class="slider">
-	<li><img src="/exhibition/img/image1.jpg"/></li>
+	<li>
+		<article>
+			<div class="captioncontainer">
+			<div class="caption">
+				<h1>Sérgio Matos</h1>
+				<p>Balaio Chair  | 2008</p>
+			</div>
+			</div>
+			<img src="/exhibition/img/image2.jpg"/>
+		</article>
+	</li>
 <ul>
 <?php require('footer.php');?>
