1dc5029d93d541dd4c2e7c6a67289e94cbc2debd
Paulo Barcelos <paulo@azucrina.org>
2012-01-05 22:58:32 +0100

    Added press

---
+++ exhibition/en/press/index.php
@@ -0,0 +1,60 @@
+
+<?php require('header.php');?>
+
+<div class="content">
+	<div class="leftcolumn">
+		<h2>Donwloads</h2>
+		
+		<ul>
+			<li>
+				<a href='#' class="download">Exhibition Press Release English (pdf)</a>
+			</li>
+			<li>
+				<a href='#' class="download">Modern Press Images (zip)</a>
+			</li>
+			<li>
+				<a href='#' class="download">Exhibition Press Release English (pdf)</a>
+			</li>
+			<li>
+				<a href='#' class="download">Modern Press Images (zip)</a>
+			</li>
+			<li>
+				<a href='#' class="download">Modern Press Images (zip)</a>
+			</li>
+			<li>
+				<a href='#' class="download">Exhibition Press Release English (pdf)</a>
+			</li>
+			<li>
+				<a href='#' class="download">Exhibition Press Release English (pdf)</a>
+			</li>
+			<li>
+				<a href='#' class="download">Modern Press Images (zip)</a>
+			</li>
+		</ul>
+		
+	</div>
+
+	<div class="rightcolumn">
+
+		<h2>Press Contact</h2>
+		
+		<p>Anne Maier PR<br/>
+			<a href="mailto:a™@a.com">annemaier@berlin.de</a><br/>
+			+49 (0) 5555.555555 (mobile)<br/>
+			+49 (0) 5555.555555 (mobile)<br/>
+			+49 (0) 5555.555555 (mobile)<br/>
+		</p>
+
+		<h2>Press Contact</h2>
+		
+		<p>Anne Maier PR<br/>
+			<a href="mailto:a™@a.com">annemaier@berlin.de</a><br/>
+			+49 (0) 5555.555555 (mobile)<br/>
+			+49 (0) 5555.555555 (mobile)<br/>
+			+49 (0) 5555.555555 (mobile)<br/>
+		</p>
+		
+	</div>
+</div>
+
+<?php require('footer.php');?>
--- exhibition/js/script.js
+++ exhibition/js/script.js
@@ -4,22 +4,24 @@
 
 
 $(function(){
-	var firstItem;
-	var previousItem;
-	var lis = $('.slider').find('li');
-	lis.each(function(i){
-		$(this).hide();
-		
-		if(i==0){
-			firstItem = $(this);
-		}
-		else{
-			previousItem.data('next', $(this));
-		}
-		previousItem = $(this)
-	});
-	previousItem.data('next', firstItem);
-	animateSlider(firstItem);
+	if($('.slider').length){
+		var firstItem;
+		var previousItem;
+		var lis = $('.slider').find('li');
+		lis.each(function(i){
+			$(this).hide();
+			
+			if(i==0){
+				firstItem = $(this);
+			}
+			else{
+				previousItem.data('next', $(this));
+			}
+			previousItem = $(this)
+		});
+		previousItem.data('next', firstItem);
+		animateSlider(firstItem);
+	}
 });
 
 function animateSlider(element){
