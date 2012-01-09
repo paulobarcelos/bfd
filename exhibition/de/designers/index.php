4a92124b4dba61fdb939896b561dd4a2e9109b00
Paulo Barcelos <paulo@azucrina.org>
2012-01-09 22:32:11 +0100

    Added de as a copy of en

---
+++ exhibition/de/designers/index.php
@@ -0,0 +1,76 @@
+
+<?php require('header.php');?>
+
+<div class="content">
+	<div class="leftcolumn">
+		<div class="composedtitle clearfix">
+			<h2>Modern</h2>
+			<span class="curated">Curated by Luciana Wiegmann</span>
+		</div>
+		<span class="location">Shown in zeitlos design gallery</span>
+		
+		<ul>
+			<li>
+				<a href='http://en.wikipedia.org/wiki/Joaquim_Tenreiro' target="_blank">Joaquim Tenreiro</a>
+			</li>
+			<li>
+				<a href='http://espasso.com/biography.asp?id=10' target="_blank">Jorge Zalszupin</a>
+			</li>
+			<li>
+				<a href='http://www.r20thcentury.com/biography_detail.cfm?designer_id=39' target="_blank">José Zanine Caldas</a>
+			</li>
+			<li>
+				<a href='http://www.institutobardi.com.br/eng/lina/biografia/index.html' target="_blank">Lina Bo Bardi</a>
+			</li>
+			<li>
+				<a href='http://en.wikipedia.org/wiki/Oscar_Niemeyer' target="_blank">Oscar Niemeyer</a>
+			</li>
+			<li>
+				<a href='http://en.wikipedia.org/wiki/Paulo_Mendes_da_Rocha' target="_blank">Paulo Mendes da Rocha</a>
+			</li>
+			<li>
+				<a href='http://www.sergiorodrigues.com.br/english/index.asp' target="_blank">Sergio Rodrigues</a>
+			</li>
+		</ul>
+		
+	</div>
+
+	<div class="rightcolumn">
+
+		<div class="composedtitle clearfix">
+			<h2>Contemporary</h2>
+			<span class="curated">Curated by Zanini de Zanine</span>
+		</div>
+		<span class="location">Shown in the ground floor of stilwerk Mall</span>
+		
+		<ul>
+			<li>
+				<a href='http://www.brunnojahara.com/info.html' target="_blank">Bruno Jahara</a>
+			</li>
+			<li>
+				<a href='http://www.campanas.com.br/home_en.html' target="_blank">Campana Brothers</a>
+			</li>
+			<li>
+				<a href='http://www.carlosmotta.com.br/index2.asp?lingua=eng' target="_blank">Carlos Motta</a>
+			</li>
+			<li>
+				<a href='http://www.domingostotora.com.br/' target="_blank">Domingos Tótora</a>
+			</li>
+			<li>
+				<a href='http://www.ovo.art.br/sobre_a_ovo.php ' target="_blank">,Ovo</a>
+			</li>
+			<li>
+				<a href='http://rodrigoalmeidadesign.com/2008/Rodrigo_Almeida.html' target="_blank">Rodrigo Almeida</a>
+			</li>
+			<li>
+				<a href='http://sergiojmatos.blogspot.com/' target="_blank">Sergio Matos</a>
+			</li>
+			<li>
+				<a href='http://www.studiozanini.com.br/' target="_blank">Zanini de Zanine</a>
+			</li>
+		</ul>
+		
+	</div>
+</div>
+
+<?php require('../footer.php');?>
---
+++ exhibition/de/sponsors/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Sponsors - Brazilian Design - Modern &amp; Contenporary Furniture </title>
+
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <link rel="stylesheet" href="/exhibition/css/style.css">
+  <link href="http://fast.fonts.com/cssapi/6b71cd19-d93a-4402-be6f-0a2a9c58ac1d.css" rel="stylesheet" type="text/css" />
+
+  <script src="/exhibition/js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body class="en">
+  <header id="header" class="clearfix">
+
+    <h1 class="logo ir">Brazilian Design</h1>
+
+    <nav id="mainnav">
+      <ul>
+        <li><a href="/exhibition/en/">Home</a></li>
+        <li><a href="/exhibition/en/press/">Press</a></li>
+        <li><a href="/exhibition/en/designers/">Designers</a></li>
+        <li class="current"><a href="/exhibition/en/sponsors/">Sponsors</a></li>
+        <li class="language"><a href="/exhibition/de/">Deustche</a></li>
+      </ul>
+    </nav>
+
+  </header>
+  <div role="main" id="main">
