5116bd343266c6326a60c49b5db281fa4b62acfa
Paulo Barcelos <paulo@azucrina.org>
2012-03-11 21:09:44 +0100

    Removed exhibition specific

--- exhibition/de/video/index.php
+++
@@ -1,8 +0,0 @@
-
-<?php require('header.php');?>
-
-<div class="videoplayer">
-	<iframe width="100%" height="100%" src="http://www.youtube.com/embed/lEbn7Da9e7w" frameborder="0" allowfullscreen></iframe>
-</div>
-
-<?php require('../footer.php');?>
